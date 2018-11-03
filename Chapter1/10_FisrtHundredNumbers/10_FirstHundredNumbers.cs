06507d5f3ed20a7c8eaab2877021b048636e92a3
Aycan45 <43782112+Aycan45@users.noreply.github.com>
2018-11-03 18:36:20 +0200

    Add files via upload

---
+++ Chapter1/10_FisrtHundredNumbers/10_FirstHundredNumbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        sbyte i = sbyte.Parse(Console.ReadLine());
+        for (i = 0; i < 100; i++)
+        {
+            Console.WriteLine($"The numbers of i are {i}");
+        }
+    }
+}
+
---
+++ Chapter1/11_MyAgeAfter10Years/11_MyAgeAfter10Years.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        byte myAge = 19;
+        int MyAgeAfter10Years = myAge + 10;
+        Console.WriteLine($"My age after 10 years will be {MyAgeAfter10Years}");
+    }
+}
+
